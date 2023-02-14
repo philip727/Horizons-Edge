afe9c4f54859a286000e04d9d6e320840fcaa722
philip <philster36@hotmail.co.uk>
2023-02-14 22:38:29 +0000

    new chunk renderer, keeps chunks in memory

--- Assets/Scripts/Utilities/Philip Utilities/World Generation/ChunkUpdater.cs
+++ Assets/Scripts/Utilities/Philip Utilities/World Generation/ChunkUpdater.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.Linq;
+using Unity.Burst;
 using UnityEngine;
 
 namespace Philip.WorldGeneration
@@ -20,7 +22,9 @@ namespace Philip.WorldGeneration
         //private readonly List<ChunkNode> _loadedChunksLastUpdate = new List<ChunkNode>();
         //private readonly List<ChunkNode> _chunksAlreadyLoadedOnFrame = new List<ChunkNode>();
 
-        public readonly Dictionary<Vector2Int, ChunkData> chunksLoaded = new Dictionary<Vector2Int, ChunkData>();
+        public readonly Dictionary<Vector2Int, ChunkData> _chunksThatHaveBeenCreated = new Dictionary<Vector2Int, ChunkData>();
+        public readonly Dictionary<Vector2Int, ChunkData> _chunksCurrentlyRendered = new Dictionary<Vector2Int, ChunkData>();
+        public readonly Dictionary<Vector2Int, ChunkData> _chunksReceivedToRenderThisFrame = new Dictionary<Vector2Int, ChunkData>();
 
         private void Start()
         {
@@ -39,21 +43,66 @@ namespace Philip.WorldGeneration
             int chunkY = Mathf.FloorToInt(s_viewerPosition.y / _worldGenerationSettings.ChunkSize);
             Vector2Int bottomLeftOfCurrentChunk = new Vector2Int(chunkX * _worldGenerationSettings.ChunkSize, chunkY * _worldGenerationSettings.ChunkSize);
 
+            // Clear the chunks rendered from last frame as we don't want to check them in this dict
+            _chunksReceivedToRenderThisFrame.Clear();
+
             for (int x = -MAX_VIEW_DISTANCE; x < MAX_VIEW_DISTANCE; x++)
             {
                 for (int y = -MAX_VIEW_DISTANCE; y < MAX_VIEW_DISTANCE; y++)
                 {
+                    // The current chunk coordinates
                     Vector2Int bottomLeftOfChunkInViewDist = new Vector2Int(bottomLeftOfCurrentChunk.x + (x * _worldGenerationSettings.ChunkSize),
                                                                             bottomLeftOfCurrentChunk.y + (y * _worldGenerationSettings.ChunkSize));
-                    if (chunksLoaded.ContainsKey(bottomLeftOfChunkInViewDist)) continue;
-                    ChunkData chunkData = _worldGenerationHandler.RequestChunkData(bottomLeftOfChunkInViewDist.x, bottomLeftOfChunkInViewDist.y);
-                    CreateChunkFromData(chunkData);
-                    chunksLoaded.Add(bottomLeftOfChunkInViewDist, chunkData);
-                }
 
+                    ChunkData chunkData;
+                    if (!_chunksThatHaveBeenCreated.ContainsKey(bottomLeftOfChunkInViewDist))
+                    {
+                        // At this point the chunk is in the view distance but has never been created
+                        chunkData = _worldGenerationHandler.RequestChunkData(bottomLeftOfChunkInViewDist.x, bottomLeftOfChunkInViewDist.y);
+                        CreateChunkFromData(chunkData);
+                        _chunksThatHaveBeenCreated.Add(bottomLeftOfChunkInViewDist, chunkData);
+                    }
+                    else
+                    {
+                        // The chunk has been created before and is now in the view distance
+                        _chunksThatHaveBeenCreated.TryGetValue(bottomLeftOfChunkInViewDist, out chunkData);
+
+                        // If it's not visible, then we should render it
+                        if(!chunkData.IsVisible)
+                        {
+                            chunkData.SetVisible(true);
+                        }
+
+
+                    }
+
+                    if(!_chunksReceivedToRenderThisFrame.ContainsKey(bottomLeftOfChunkInViewDist))
+                    {
+                        // If it's in the view distance then it must have been requested to render
+                        _chunksReceivedToRenderThisFrame.Add(bottomLeftOfChunkInViewDist, chunkData);
+                    }
+
+                    if(!_chunksCurrentlyRendered.ContainsKey(bottomLeftOfChunkInViewDist))
+                    {
+                        // All rendered chunks will be added to this dict since we need to keep track of what needs to be unrendered
+                        _chunksCurrentlyRendered.Add(bottomLeftOfChunkInViewDist, chunkData);
+                    }
+                }
+            }
+
+            // If it's no longer in the view distance but is still rendered, then unrender it
+            for (int i = _chunksCurrentlyRendered.Count - 1; i >= 0; i--)
+            {
+                KeyValuePair<Vector2Int, ChunkData> chunkByCoordinate = _chunksCurrentlyRendered.ElementAt(i);
+                if (!_chunksReceivedToRenderThisFrame.ContainsKey(chunkByCoordinate.Key))
+                {
+                    chunkByCoordinate.Value.SetVisible(false);
+                    _chunksCurrentlyRendered.Remove(chunkByCoordinate.Key);
+                }
             }
         }
 
+        [BurstCompile]
         private ChunkData CreateChunkFromData(ChunkData chunkData)
         {
             GameObject newChunkPrefab = Instantiate(ChunkPrefab, new Vector3(chunkData.Coordinates.x, chunkData.Coordinates.y, 0f), Quaternion.identity, transform);
